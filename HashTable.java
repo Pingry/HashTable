64b9ca9488d528b54e7ea9d1f729202553f4f1cc
Zachary Keller <zkeller2017@pingry.org>
2015-09-26 13:49:14 -0400

    finshed put method without reHash, compiles

--- HashTable.java
+++ HashTable.java
@@ -1,16 +1,39 @@
 public class HashTable
 {
-	private int capacity;
-	private static FINAL double loadFactor = .6;
+	public static final double loadFactor = .6;
+	private Object[] table;
 	
 	public HashTable()
 	{
-		capacity=100;
+		table = new Object[100];
 	}
 	
 	public HashTable(int c)
 	{
-		capacity= c;
+		table = new Object[c];
+	}
+	
+	public void put(Object o)
+	{
+		int spot = (int)  o.hashCode();
+		spot=spot% table.length;
+		int change=0;
+		boolean placed= false;
+		while( placed == false)
+		{
+			if (!(table[spot + change] instanceof Integer ))
+				change+=1;
+			else
+			{
+				table[spot + change] = o;
+				placed= true;
+			}
+			if (change+spot>table.length)
+				change= spot * -1;
+		}
+		
+		
+		
 	}
 	
 	
