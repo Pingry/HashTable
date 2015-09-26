ec61784c33e2405d0ad3e379409948b6d9f9bd8f
Zachary Keller <zkeller2017@pingry.org>
2015-09-26 12:23:25 -0400

    First commit, hardly anything done

---
+++ HashTable.java
@@ -0,0 +1,21 @@
+public class HashTable
+{
+	private int capacity;
+	private static FINAL double loadFactor = .6;
+	
+	public HashTable()
+	{
+		capacity=100;
+	}
+	
+	public HashTable(int c)
+	{
+		capacity= c;
+	}
+	
+	
+
+
+
+
+}
