6a59ea58bd5aee6b8ee2d57f436cf50bca503c75
Zachary Keller <zkeller2017@pingry.org>
2015-09-26 14:02:58 -0400

    finshed rehash, added rehash into put, complies. In theory it is now done, I will test it out with a runner

--- HashTable.java
+++ HashTable.java
@@ -2,15 +2,18 @@ public class HashTable
 {
 	public static final double loadFactor = .6;
 	private Object[] table;
+	private int occupied;
 	
 	public HashTable()
 	{
 		table = new Object[100];
+		occupied=0;
 	}
 	
 	public HashTable(int c)
 	{
 		table = new Object[c];
+		occupied=0;
 	}
 	
 	public void put(Object o)
@@ -27,15 +30,32 @@ public class HashTable
 			{
 				table[spot + change] = o;
 				placed= true;
+				occupied+=1;
 			}
 			if (change+spot>table.length)
 				change= spot * -1;
 		}
+		double filled= ((double) occupied)/table.length;
+		if (filled>loadFactor)
+			rehash();
 		
 		
 		
 	}
-	
+	 private void rehash()
+	 {
+	 	Object [] holder = new Object[table.length];
+	 	for (int i=0; i<table.length; i++)
+	 	{
+	 		holder[i]=table[i];
+	 	}
+	 	table= new Object[table.length * 2];
+	 	for ( int j=0; j<holder.length; j++)
+	 	{
+	 		if (holder[j] instanceof Integer)
+	 			this.put(holder[j]);
+	 	}
+	 }
 	
 
 
