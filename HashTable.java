0b87d53ee79697816d3bbd2fadcbed0402d55bb0
Zachary Keller <zkeller2017@pingry.org>
2015-09-26 15:11:01 -0400

    Everything is working, done as far as I know

--- HashTable.java
+++ HashTable.java
@@ -19,14 +19,16 @@ public class HashTable
 	public void put(Object o)
 	{
 		int spot = (int)  o.hashCode();
+		System.out.println("Intial code is: " + spot);
 		spot=spot% table.length;
+		System.out.println("Code after mod is: " + spot);
 		int change=0;
 		boolean placed= false;
 		while( placed == false)
 		{
-			System.out.println("change: " + change)
-			System.out.println("spot: " + spot)
-			if (!(table[spot + change] instanceof Integer ))
+			System.out.println("change: " + change);
+			System.out.println("spot: " + spot);
+			if (table[spot + change] instanceof Object )
 				change+=1;
 			else
 			{
@@ -34,18 +36,29 @@ public class HashTable
 				placed= true;
 				occupied+=1;
 			}
-			if (change+spot>table.length)
+			if (change+spot==table.length)
 				change= spot * -1;
 		}
+		/*
+		System.out.println("");
+		System.out.println(this.toString());
+		System.out.println("");
+		System.out.println("Occupied: " + occupied);
+		System.out.println("");
+		*/
 		double filled= ((double) occupied)/table.length;
 		if (filled>loadFactor)
+		{
+			occupied=0;
 			rehash();
+		}
 		
 		
 		
 	}
 	 private void rehash()
 	 {
+	 	System.out.println("Rehashing");
 	 	Object [] holder = new Object[table.length];
 	 	for (int i=0; i<table.length; i++)
 	 	{
@@ -54,7 +67,7 @@ public class HashTable
 	 	table= new Object[table.length * 2];
 	 	for ( int j=0; j<holder.length; j++)
 	 	{
-	 		if (holder[j] instanceof Integer)
+	 		if (holder[j] instanceof Object)
 	 			this.put(holder[j]);
 	 	}
 	 }
--- Runner.java
+++ Runner.java
@@ -3,11 +3,9 @@ public class Runner
 
 	public static void main( String [] args )
 	{
-		HashTable h= new HashTable(4);
-		h.put(new Object());
-		h.put(new Object());
-		h.put(new Object());
-		h.put(new Object());
+		HashTable h= new HashTable(Integer.parseInt(args[0]));
+		for (int i=0; i<Integer.parseInt(args[1]); i++)
+			h.put(new Object());
 		System.out.println(h.toString());
 	}
 
