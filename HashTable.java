e197466b48806eebb0a87957b3f7e9dc4b26c3f9
Zachary Keller <zkeller2017@pingry.org>
2015-09-29 21:31:51 -0400

    this is actually my last commit this time. Ready to be graded, with javadoc comments and everything

--- HashTable.java
+++ HashTable.java
@@ -1,67 +1,83 @@
+/** This is my hashTable class. It creates and modifies the actual table
+	that objects can be added to. It uses the code to place the object in the table
+	
+	@author Zachary Keller
+	@version 9/29/15
+*/
+
+
 import java.lang.Math;
 
 public class HashTable
 {
+/** Any percentage of occupancy above this is too much and will rehash */
 	public static final double loadFactor = .6;
+/** the actual table that the objects are being appended to*/
 	private Object[] table;
+/** the number of objects in the table, is used to determine whether the loadFactor is being exceeded*/
 	private int occupied;
-	//occupied is the number of spaces in the table that have been taken
 
-//Default constructor. Initializes to capacity 100.	
+/** Default constructor. Initializes the table to capacity 100.	*/
 	public HashTable()
 	{
 		table = new Object[100];
 		occupied=0;
 	}
 	
+/** Constructor with table length parameter
+	@param c the length of the hashTable to be created
+*/
 	public HashTable(int c)
 	{
 		table = new Object[c];
 		occupied=0;
 	}
 
-//Puts the object in the hashtable. 
-//Deals with collisions by placing the object using quadratic probing
 
+/** Puts the object in the hashtable. 
+	Deals with collisions by placing the object using quadratic probing
+	@param o  an object to be placed in the table
+*/
 	public void put(Object o)
 	{
 		int spot = (int)  o.hashCode();
-		System.out.println("Intial code is: " + spot);
-		spot=spot% table.length;
+		// System.out.println("Intial code is: " + spot);
+		spot = spot% table.length;
 		spot = Math.abs(spot);
-		System.out.println("Code after mod is: " + spot);
-		int change=0;
-		boolean placed= false;
+		// System.out.println("Code after mod is: " + spot);
+		// At this point, spot represents where the object will be placed
+		// and change is how far the spot will be moved if there is an
+		// object already in its spot
+		int change = 0;
+		boolean placed = false;
 		while(placed == false)
 		{
-			int place=(spot + change)%table.length;
-			System.out.println("change: " + change);
-			System.out.println("spot: " + spot);
+			int place= (spot + change) % table.length;
 			if (table[place] instanceof Object )
 			{
-				if (change==0)
-					change+=1;
+				if (change == 0)
+					change += 1;
 				else
-					change=change*2;
+					change = change * 2;
 			}
 			else
 			{
 				table[place] = o;
-				placed= true;
-				occupied+=1;
+				placed = true;
+				occupied += 1;
 			}
 		}
-		/*
+		/* USED FOR TESTING PURPOSES ONLY
 		System.out.println("");
 		System.out.println(this.toString());
 		System.out.println("");
 		System.out.println("Occupied: " + occupied);
 		System.out.println("");
 		*/
-		double filled= ((double) occupied)/table.length;
-		if (filled>loadFactor)
+		double filled = ((double) occupied) / table.length;
+		if (filled > loadFactor)
 		{
-			occupied=0;
+			occupied = 0;
 			rehash();
 		}
 		
@@ -69,20 +85,20 @@ public class HashTable
 		
 	}
 	
-//Makes the size of the HashTable the next prime number after double the size of the previous one
-// and rehashes each item contained within. 
+/** Makes the size of the HashTable the next prime number after double the size of the previous one
+	and calls put for each item contained within the old hashTable
+*/ 
 	 private void rehash()
 	 {
-	 	System.out.println("Rehashing");
 	 	Object [] holder = new Object[table.length];
 	 	for (int i=0; i<table.length; i++)
 	 	{
-	 		holder[i]=table[i];
+	 		holder[i] =table[i];
 	 	}
-	 	int biggerPrime= (table.length * 2) +1;
-	 	while (primeChecker(biggerPrime)==false)
-	 		biggerPrime+=2;
-	 	table= new Object[biggerPrime];
+	 	int biggerPrime = (table.length * 2) + 1;
+	 	while (primeChecker(biggerPrime) == false)
+	 		biggerPrime += 2;
+	 	table = new Object[biggerPrime];
 	 	for ( int j=0; j<holder.length; j++)
 	 	{
 	 		if (holder[j] instanceof Object)
@@ -90,25 +106,37 @@ public class HashTable
 	 	}
 	 }
 	
-//checks for the next prime, called in rehash 
-//not so efficient
+/** This determines whether a given number is a prime number.
+	Admittedly, this algorithm is incredibly inefficient,
+	but I did not really know any better ways of doing this.
+	@param a the number to be checked fo whether it is prime
+	@return whether or not the parameter is prime
+*/
 	 private boolean primeChecker(int a)
 	 {
-	 	for (int x=2; x<((int) Math.sqrt(a))+1; x++)
+	 	for (int x=2; x<((int) Math.sqrt(a)) + 1; x++)
 	 	{
-	 		if (a%x==0)
+	 		if (a%x == 0)
 	 			return false;
 	 	}
 	 	return true;
 	 }
 
-//String representation of the HashTable.	 
+/** Returns a string representation of the hash table, 
+	complete with aesthetically pleasing dividers.
+
+*/	 
 	 public String toString()
 	 {
-	 	String s= "";
+	 	String s= "|";
 	 	for (int i=0; i<table.length; i++)
 	 	{
-	 		s= s+ table[i] + "|";
+	 		// so that we can see the actual object, as opposed to its address
+	 		if (table[i] instanceof Object)
+	 			s= s+ table[i].toString();
+	 		else
+	 			s= s+ table[i];
+	 		s=s+ "|";
 	 	}
 	 	return s;
 	 }
--- Runner.java
+++ Runner.java
@@ -1,16 +1,31 @@
+/** This is my runner, I use this to make sure everything in my HashTable class
+	is running smoothly
+	
+	@author Zachary Keller
+	@version 9/29/15
+*/
+
 public class Runner
 {
 
+/** Here in my main, I create a new hashTable and then populate it with
+	objects. I hardcoded the length of 7 in because origninally I had that be
+	args[0] but I found that to be kind of a hassle to work with. Same thing for the 
+	justification of using 10 objects to populate the table. I do make sure to put in enough
+	that the table has to rehash.
+	@param args 
+	
+*/
 	public static void main( String [] args )
 	{
 		HashTable h= new HashTable(7);
 		for (int i=0; i<5; i++)
 		{
 			h.put(new Object());
-			h.put("hello");
+			h.put("testString");
 		}
 
-		System.out.println(h);
+		//System.out.println(h);  -used to see the final table
 	}
 
 			
