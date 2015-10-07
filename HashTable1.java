45c33724329ed046ee5d1256b6406f9356ae43a9
Sam Scherl <sscherl2017@pingry.org>
2015-10-07 19:52:49 -0400

    Final commit, added occupied variable, fixed some other minor bugs

--- HashTable1.java
+++ HashTable1.java
@@ -1,25 +1,25 @@
 import java.lang.Math;
 
 /** 
- * This class is a hash table to store objects and contains several methods to interact with the hash table. It
- * also uses generics and key, value associations to enable easy accessing, adding, and removing of items in the hash table.
+ * This class is a hash table to store objects and contains several methods to interact with the hash table. it
+ * also uses generics and key, value associations to enable easy accessing, adding, and removing of items in the hash table
  * @author Sam Scherl
  * @version 10.3.15
  */
-public class HashTable<K, V>
+public class HashTable1<K, V>
 {
 	private final double loadFactor = .5;
 	
 	/** an array of type Entry which will function as a hash table */
 	private Entry[] table;
 	
-	/** an int which represents the number of Entries in the hash table */
+	/** an int which represents the number of Entries in the hash table*/
 	private double occupied;
 	
 	/** 
 	* Default constructor which sets size of the hash table to 100.
 	*/
-	public HashTable()
+	public HashTable1()
 	{
 		table = new Entry[100];
 		occupied = 0;
@@ -29,14 +29,15 @@ public class HashTable<K, V>
 	* Constructor which sets the initial size of the hash table to the given capacity.
 	* @param capacity    the initial capacity of the array
 	*/
-	public HashTable(int capacity)
+	public HashTable1(int capacity)
 	{
 		table = new Entry[capacity];
 		occupied = 0;
 	}
 	
 	/**
-	* Create a new Entry object which contains both the key and the value. Puts the Entry in the hash table based off the key's hash code. 
+	* Create a new Entry object which contains both the key and the value.
+	* Puts the Entry in the hash table based off the key's hash code. 
 	* Deals with collisions by placing the Entry in the next open spot.
 	* If the percentage of the hash table which is full is above the loadFactor it will double the size of the hash table and rehash the every Entry within.
 	* If the key previously exists within the hash table it will replace the old Entry containing the key wih the new Entry containing the key.
@@ -47,19 +48,13 @@ public class HashTable<K, V>
 	{
 		Entry input = new Entry(key, value);
 		if (table[Math.abs(input.key.hashCode()) % table.length] == null || ((K) table[Math.abs(input.key.hashCode()) % table.length].key).equals(key))
-		{
-			if (table[Math.abs(input.key.hashCode()) % table.length] == null)
-				occupied++;
 			table[Math.abs(input.key.hashCode() % table.length)] = input;
-		}
 		else
 		{
 		//displacement is used to increment through the hash table, away from the original location which key.hashCode() points to
 			int displacement = 0;
-			while (table[(Math.abs(input.key.hashCode()) + displacement) % table.length] != null && !((K) table[(Math.abs(input.key.hashCode()) + displacement) % table.length].key).equals(key))
+			while (table[(Math.abs(input.key.hashCode()) + displacement) % table.length] != null)
 				displacement += 1;
-			if (table[(Math.abs(input.key.hashCode()) + displacement) % table.length] == null)
-				occupied++;
 			table[(Math.abs(input.key.hashCode()) + displacement) % table.length] = input;
 		}		
 		if (occupied / table.length >= loadFactor)
@@ -68,7 +63,7 @@ public class HashTable<K, V>
 	
 	/**
 	* Creates a String representation of the HashTable. with the each index next to its key and value.
-	* @return	the string which contains a visualization of the hash table
+	* @return	the string which contains a visualization of the hashtable
 	*/
 	public String toString()
 	{
@@ -106,18 +101,16 @@ public class HashTable<K, V>
 	*/
 	public V remove(K key)
 	{
-		int keyLocation = findKey(key);
-		if (keyLocation == -1)
+		if (findKey(key) == -1)
 			return null;
 		occupied--;
-		V output = (V) table[(Math.abs(key.hashCode()) + keyLocation) % table.length].value;
-		table[(Math.abs(key.hashCode()) + keyLocation) % table.length] = null;
-		int displacement = keyLocation + 1;;
+		V output = (V) table[(Math.abs(key.hashCode()) + findKey(key)) % table.length].value;
+		table[(Math.abs(key.hashCode()) + findKey(key)) % table.length] = null;
+		int displacement = findKey(key) + 1;;
 		while (table[(Math.abs(key.hashCode()) + displacement) % table.length] != null)
 		{
 			Entry holder = new Entry((K) table[(Math.abs(key.hashCode()) + displacement) % table.length].key, (V) table[(Math.abs(key.hashCode()) + displacement) % table.length].value);
 			table[(Math.abs(key.hashCode()) + displacement) % table.length] = null;
-			occupied--;
 			put((K) holder.key, (V) holder.value);
 			displacement++;
 			
