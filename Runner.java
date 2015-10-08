ceb15ad613387a17bd899e91c9b549cb54f5f9b7
aseth2017 <aseth2017@pingry.org>
2015-10-08 06:17:12 -0400

    Version 1 Final

--- Runner.java
+++ Runner.java
@@ -2,12 +2,14 @@ public class Runner
 {
 	public static void main(String[] args)
 	{
-		HashTable table = new HashTable(70);
-		table.put("English");
-		table.put("Math");
-		table.put("Science");
-		table.put("Computer Science");
-		table.put("Spanish");
+		HashTable<Integer, String> table = new HashTable<Integer, String>(10);
+		table.put(1, "1 Item");
+		table.put(2, "2 Item");
+		table.put(3, "3 Item");
+		table.put(4, "4 Item");
+		table.put(5, "5 Item");
+		table.remove(2);
+		
 		System.out.println(table);
 	}
 }
