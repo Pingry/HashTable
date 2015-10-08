81a1c22bd849730732698c5db4106e5085add695
Zachary Keller <zkeller2017@pingry.org>
2015-10-07 20:14:27 -0400

    Final copy of Version 1

--- HashTable.java
+++ HashTable.java
@@ -1,21 +1,37 @@
+import java.lang.Math;
+import java.util.Arrays;
+
 /** This is my hashTable class. It creates and modifies the actual table
-	that objects can be added to. It uses the code to place the object in the table
+	that entries can be added to. The hashTable class contains put and remove
+	functions that works in constant time, and a number of other helpful methods.
 	
 	@author Zachary Keller
-	@version 9/29/15
+	@version 1 Final- 10/7/15
 */
 
 
-import java.lang.Math;
-
+/** The HashTable Class, takes in two generics that eventually are used to create
+	The entry
+*/
 public class HashTable<K,V>
 {
-/** Any percentage of occupancy above this is too much and will rehash */
-	public static final double loadFactor = .6;
-/** the actual table that the objects are being appended to*/
+/** Any percentage of occupancy above this is too much and will rehash.
+	A modifier for this is below */
+	private double loadFactor = .6;
+	
+/** the actual table that the objects are being appended to */
 	private Entry[] table;
-/** the number of objects in the table, is used to determine whether the loadFactor is being exceeded*/
+	
+/** the number of objects in the table, is used to determine whether the loadFactor is being exceeded */
 	private int occupied;
+	
+/** Used to calculate a prime number length of the table so that quadratic probing can work.
+	Utilized in my primeChecker function and its helpers
+	I made the length of the array such because that seems like an arbitrarily large enough number
+	for the table to every possibly be, but it can be any number that the array can handle
+	It has to be this way for the way I implemented the prime checker
+*/
+	private boolean[] primes = new boolean[10000];
 
 /** Default constructor. Initializes the table to capacity 100.	*/
 	public HashTable()
@@ -32,11 +48,22 @@ public class HashTable<K,V>
 		table = new Entry[c];
 		occupied=0;
 	}
+	
+/** Sets the load factor to some new percentage that the user desires,
+	only works if the factor is less than 1 to prevent errors
+	@param a The new loadFactor
+*/	
+	public void setLoadFactor(double a)
+	{
+		if (a < 1)
+			loadFactor = a;
+	}
 
 
 /** Puts the object in the hashtable. 
 	Deals with collisions by placing the object using quadratic probing
-	@param o  an object to be placed in the table
+	@param k  The key, of type Generic K, that will be used to set the key of the entry
+	@param v  The value, of type Generic V, that will be used to set the value of the entry
 */
 	public void put(K k, V v)
 	{
@@ -70,13 +97,6 @@ public class HashTable<K,V>
 				occupied += 1;
 			}
 		}
-		/* USED FOR TESTING PURPOSES ONLY
-		System.out.println("");
-		System.out.println(this.toString());
-		System.out.println("");
-		System.out.println("Occupied: " + occupied);
-		System.out.println("");
-		*/
 		double filled = ((double) occupied) / table.length;
 		if (filled > loadFactor)
 		{
@@ -98,10 +118,9 @@ public class HashTable<K,V>
 	 	{
 	 		holder[i] =table[i];
 	 	}
-	 	int biggerPrime = (table.length * 2) + 1;
-	 	while (primeChecker(biggerPrime) == false)
-	 		biggerPrime += 2;
-	 	table = new Entry[biggerPrime];
+	 	int newLen = (table.length * 2) + 1;
+	 	System.out.println(newLen);
+	 	table = new Entry[findNextPrime(newLen)];
 	 	for ( int j=0; j<holder.length; j++)
 	 	{
 	 		if (holder[j] != null)
@@ -109,21 +128,64 @@ public class HashTable<K,V>
 	 	}
 	 }
 	
-/** This determines whether a given number is a prime number.
-	Admittedly, this algorithm is incredibly inefficient,
-	but I did not really know any better ways of doing this.
-	@param a the number to be checked fo whether it is prime
-	@return whether or not the parameter is prime
+/** This determines the next prime number after a given number
+	I used a method copied from the internent to make this more efficient- see method below for more info.
+	
+	@param a the base number: it looks for the next prime after a;
+	@return the next prime number
 */
-	 private boolean primeChecker(int a)
+	 private int findNextPrime(int a)
 	 {
-	 	for (int x=2; x<((int) Math.sqrt(a)) + 1; x++)
+	 	if (primes[11] == false) // makes it so I only call fillSieve once, because primes[11] would not be false if I had already called it
 	 	{
-	 		if (a%x == 0)
-	 			return false;
+	 		fillSieve();
 	 	}
-	 	return true;
+	 	int i = a;	
+	  	while (primes[i] != true)
+	  	{
+	  		i += 1;
+	  		if (i == primes.length)
+	  		{
+	  			System.out.println("Error: rehashing has caused the new table to be to long");
+	  			System.exit(0);
+	  		}
+	  	}
+
+	  	return i;
+	  	
+	  		
 	 }
+	 
+	  
+/** This is code I copied from online (per Mr. Burkhat's recommendation). I basically starts
+	with the assumption that all numbers in the length of prime array (see class fields above) are prime,
+	and then eliminates ones that aren't by changing their value from true to false
+	I made some changes but the original code can be found here: 
+	http://www.mkyong.com/java/how-to-determine-a-prime-number-in-java/
+*/
+	private void fillSieve() 
+	{
+    	Arrays.fill(primes,true);        // assume all integers are prime.
+    	primes[0]=primes[1]=false;       // we know 0 and 1 are not prime.
+    	for (int i=2;i<primes.length;i++)
+    	{
+       		//if the number is prime, 
+        	//then go through all its multiples and make their values false.
+        	if (primes[i])
+    		{
+            	for (int j=2;i*j<primes.length;j++)
+            	{
+                	primes[i*j]=false;
+            	}
+        	}
+    	}
+	}
+
+/** This code removes the entry from the table that has the given key, and then adjusts the
+	table to make sure the removal of the one entry does not affect any others
+	@param key  The key of the entry that is to be removed
+	@return the value of the entry with the given key
+*/
 	 
 	 public V remove(K key)
 	 {
@@ -131,8 +193,8 @@ public class HashTable<K,V>
 	 	spot = spot% table.length;
 		spot = Math.abs(spot);
 		int finder=0;
-		int place= spot + finder%table.length;
-		while ( table[place]!= null &&  (K) (table[place].getKey()) != key)
+		int place= (spot + finder) % table.length;
+		while ( table[place] != null &&  (K) (table[place].getKey()) != key)
 		{
 			
 			if (finder == 0)
@@ -140,24 +202,39 @@ public class HashTable<K,V>
 			else
 				finder = finder * 2;
 				
-			place= spot + finder % table.length;
+			place= (spot + finder) % table.length;
 				
 				
 		}
-		if (table[place]==null)
+		if (table[place] == null)
 		{
 			return null;
 		}
 		
-		
 		Entry<K,V> temp = table[place];
 		V output = temp.getValue();
-		System.out.println(output.getClass().getName());
-		table[place]= null;
+		//actually removes the entry here
+		table[place] = null;
+		//now the rest of this function is just to reset the other entries
+		//that may have been affected by the removal
+		finder = finder * 2;
+		place = (spot + finder) % table.length;
+		while (table[place] != null)
+		{
+			put( (K) table[place].getKey() , (V) table[place].getValue());
+			finder = finder * 2;
+			place = (spot + finder) % table.length;
+		}
+		//because there is one fewer item in the table now
 		occupied -= 1;
 		return output;
 		
 	 }
+	 
+/** returns the value of an entry with the given key
+	@param key  The key of the entry that is desired
+	@return the value of the entry with the given key
+*/
 
 	public V get(K key)
 	{
@@ -165,7 +242,7 @@ public class HashTable<K,V>
 	 	spot = spot% table.length;
 		spot = Math.abs(spot);
 		int finder=0;
-		int place= spot + finder%table.length;
+		int place= (spot + finder) %table.length;
 		while ( table[place]!= null &&  (K) (table[place].getKey()) != key)
 		{
 			
@@ -174,11 +251,11 @@ public class HashTable<K,V>
 			else
 				finder = finder * 2;
 				
-			place= spot + finder % table.length;
+			place= (spot + finder) % table.length;
 				
 				
 		}
-		if (table[place]==null)
+		if (table[place] == null)
 		{
 			return null;
 		}
@@ -190,6 +267,11 @@ public class HashTable<K,V>
 		
 	}
 	
+/** determines whether or not at least one entry with the
+	given value exists in the hash table
+	@param value  the value that is to be checked against the table
+	@return whether or not the value exists in the table
+*/
 	public boolean containsValue(V value)
 	{
 		for (int i=0; i<table.length; i++)
@@ -204,11 +286,29 @@ public class HashTable<K,V>
 		}
 		return false;
 	}
+	
+/** determines whether or not at least one entry with the
+	given key exists in the hash table
+	@param key  the key that is to be checked against the table
+	@return whether or not the key exists in the table
+*/
+	public boolean containsKey(K key)
+	{
+		if (get(key) == null)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	
+	}
 
 
 /** Returns a string representation of the hash table, 
 	complete with aesthetically pleasing dividers.
-
+	@return the final string with the table in it
 */	 
 	 public String toString()
 	 {
@@ -228,22 +328,34 @@ public class HashTable<K,V>
 	 	return s;
 	 }
 	 
+	 /** The entry class that fits into the hashTable
+	 	 Each entry contains a key and a value
+	 */ 
 	 private class Entry<K,V>
 	 {
-	 	private K key;
+	 	/** key stored in the object */
+	 	private K key;	
+	 	/** value stored in the object */
 	 	private V value;
 	 	
+	 	/** constructor, instantiates key and value
+	 		@param k the key of the new entry
+	 		@param v the value of the new entry
+	 	 */
 	 	public Entry(K k, V v)
 	 	{
 	 		key = k;
 	 		value = v;
 	 	}
-	 	
+	 	/** returns key
+	 		@return the key */
 	 	public K getKey()
 	 	{
 	 		return key;
 	 	}
 	 	
+	 	/** returns value
+	 		@return the value */
 	 	public V getValue()
 	 	{
 	 		return value;
--- Runner.java
+++ Runner.java
@@ -2,35 +2,39 @@
 	is running smoothly
 	
 	@author Zachary Keller
-	@version 9/29/15
+	@version 1 Final- 10/7/15
 */
-import java.util.Random;
+
 
 public class Runner
 {
 
 /** Here in my main, I create a new hashTable and then populate it with
-	objects. I hardcoded the length of 7 in because origninally I had that be
-	args[0] but I found that to be kind of a hassle to work with. Same thing for the 
-	justification of using 10 objects to populate the table. I do make sure to put in enough
-	that the table has to rehash.
-	@param args 
+	keys and values. I then test the various functions in my HashTable class,
+	and print out the actual table.
+	@param args Usual main parameter
 	
 */
 	public static void main( String [] args )
 	{
 		HashTable<Integer, String> h= new HashTable<Integer, String>(7);
 		h.put(1, "1stItem");
-		h.put(1, "2ndItem");
+		h.put(2, "2ndItem");
 		h.put(3, "3rdItem");
 		h.put(4, "4thItem");
-		h.put(5, "5thItem");;
-		System.out.println(h.remove(6));
+		h.put(5, "5thItem");
+		h.remove(2);
+		
 		System.out.println("get: " + h.get(3));
 		System.out.println("containsValue: " + h.containsValue("1stItem"));
+		System.out.println("containsKey: " + h.containsKey(1));
 
-		System.out.println(h);  //-used to see the final table
+		System.out.println( "The actual Table:  " + h);  //-used to see the final table
 	}
+
+
+
+
 
 			
 }
